34a942a373b3405985abf3612fdf6fbc59d8cebe
Gregorios <gregorios@Gregorioss-MacBook-Pro.local>
2021-10-04 15:41:21 +0200

    fix: ajout d une variable url api

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,2 +1,3 @@
 export const API_KEY_MOVIE_DB = process.env.REACT_APP_API_KEY_MOVIE_DB;
 export const URL_POSTER_PATH_MOVIE = "https://image.tmdb.org/t/p/original";
+export const URL_API_MOVIE_V3 = 'https://api.themoviedb.org/3/'
